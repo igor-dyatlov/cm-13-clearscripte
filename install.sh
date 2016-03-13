44db4996edb1b9fcd4d3a2b080ba4b171abcb809
Igor Dyatlov <dyatlov.igor@gmail.com>
2016-03-13 04:34:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-zip -r temp META-INF system
+zip -r temp ./script/META-INF ./script/system
 java -jar ./certificates/signapk.jar ./certificates/testkey.x509.pem ./certificates/testkey.pk8 temp.zip nexusfication-clearscripte-$(date +%F-%H-%M).zip
 rm temp.zip
