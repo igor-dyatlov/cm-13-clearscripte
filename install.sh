1d1477d16b1aa7709861f9a5091ecd13cc8bd9c0
Igor Dyatlov <dyatlov.igor@gmail.com>
2018-02-27 02:14:30 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 
 #
 # Copyright (C) 2016
@@ -16,6 +17,41 @@
 # limitations under the License.
 #
 
+# Current directory represents the directory where this script has been called.
+# Root directory represents the directory where this script is located.
+CRDIR=$(pwd)
+RTDIR=$(cd $(dirname ${BASH_SOURCE[0]}) && pwd)
+FSTMP="$FSDIR/.tmp"
+
 zip -r temp bullhead flo hammerhead maguro sailfish META-INF
 java -jar ./certificates/signapk.jar ./certificates/testkey.x509.pem ./certificates/testkey.pk8 temp.zip pixelization-clearscripte-2.0.1-aroma-$(date +%F-%H-%M).zip
+
+sign_flashable()
+{
+	# This function expects both a configuration to be loaded and
+	# an argument to be passed.
+	(config_loaded) || terminate "3" "Bake configuration"
+	[ ! -z $1 ] || terminate "1"
+
+	# Check the presence of the required files.
+	[ -f $SADIR/signapk.jar ] && [ -f $SADIR/keys/*.pk8 ] &&
+	[ -f $SADIR/keys/*.pem ] && [ -f $FSTMP/$1 ] || return 1
+
+	print "${CYACL}- Signing $1 archive..."
+
+	# Setup private and public keys.
+	PVKEY=$(find $SADIR/keys/*.pk8 | head -1)
+	PBKEY=$(find $SADIR/keys/*.pem | head -1)
+
+	# Sign a target flashable archive with previously set keys.
+	java -jar $SADIR/signapk.jar $PBKEY $PVKEY $FSTMP/$1 $SADIR/$1
+
+	# Move a signed archive back to the source.
+	mv -f $SADIR/$1 $FSTMP/$1
+
+	print "${GRNCL}- $1 was successfully signed!"
+}
+
+# Remove temporary flashable directory as it will be overwritten anyway.
+rm -rf $FSTMP
 rm temp.zip
